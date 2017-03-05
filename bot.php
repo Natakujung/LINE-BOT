0f90db034851bd7080a8b0a77df72b9fc58e17d6
Natakujung <natakujung@gmail.com>
2017-03-05 23:06:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,11 +15,12 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+			
+			$text = $text."....//..ไม่สำออยนะ...จากบราวนี้";
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." ไม่สำออยนะ..."
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
